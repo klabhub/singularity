530bc500af0845c7077bf0f21989fa59ff24bb49
bartkrekelberg <bart@vision.rutgers.edu>
2021-11-14 12:00:35 +0100

    .

--- nd-configurerepo.sh
+++ nd-configurerepo.sh
@@ -562,7 +562,7 @@ if LANG=C eval $ae_sudo apt-key export $nd_key_id 2>&1 1>/dev/null | grep -qe "n
     print_verbose 1 "Fetching the key from the server"
     APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
     export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE
-    eval_dry  apt-key adv --recv-keys --keyserver hkps://keyserver.ubuntu.com $nd_key_id
+    eval_dry  apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com $nd_key_id
 fi
 
 
