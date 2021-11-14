1ac821c2ab9ef61a66be5c44273b228cd73ac9a0
bartkrekelberg <bart@vision.rutgers.edu>
2021-11-14 11:42:23 +0100

    .

--- nd-configurerepo.sh
+++ nd-configurerepo.sh
@@ -20,7 +20,7 @@ set -u
 
 nd_aptenable_version=0.1
 
-nd_key_id=A5D32F012649A5A9
+nd_key_id=0xA5D32F012649A5A9
 nd_config_url=https://raw.githubusercontent.com/neurodebian/neurodebian/master/neurodebian.cfg
 nd_config_file=/etc/neurodebian/neurodebian.cfg
 nd_mirror_origin=http://neuro.debian.net/debian
@@ -562,7 +562,7 @@ if LANG=C eval $ae_sudo apt-key export $nd_key_id 2>&1 1>/dev/null | grep -qe "n
     print_verbose 1 "Fetching the key from the server"
     APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
     export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE
-    eval_dry  apt-key adv --no-tty --batch --recv-keys --keyserver hkps://keyserver.ubuntu.com $nd_key_id
+    eval_dry  apt-key adv --recv-keys --keyserver hkps://keyserver.ubuntu.com $nd_key_id
 fi
 
 
